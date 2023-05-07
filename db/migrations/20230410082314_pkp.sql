96bdd2f4b0882267390a844d4c30d24d841c3483
sofyan <sofyan@aqi.co.id>
2023-05-07 08:00:01 +0700

    update db

--- db/migrations/20230410082314_pkp.sql
+++ db/migrations/20230410082314_pkp.sql
@@ -3,30 +3,31 @@
 -- SELECT 'up SQL query';
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 CREATE TABLE public.pkp (
-    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-    stakeholders TEXT NOT NULL,
-    code TEXT NOT NULL,
-    name TEXT NOT NULL,
-    question_date TIMESTAMP WITH TIME ZONE NOT NULL,
-    question TEXT NOT NULL,
-    answers TEXT NOT NULL,
-    answers_by TEXT NOT NULL,
-    answers_at TIMESTAMP WITH TIME ZONE NOT NULL,
-    topic TEXT NOT NULL,
-    file_name TEXT NOT NULL,
-    file_path TEXT NOT NULL,
-    create_by TEXT NOT NULL,
-    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
-    updated_by TEXT NOT NULL,
-    updated_at TIMESTAMP WITH TIME ZONE NOT NULL,
-    deleted_by TEXT,
-    deleted_at TIMESTAMP WITH TIME ZONE
+id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+stakeholders TEXT NOT NULL,
+code TEXT NOT NULL,
+name TEXT NOT NULL,
+question_date TIMESTAMP WITH TIME ZONE NOT NULL,
+question TEXT NOT NULL,
+answers TEXT NOT NULL,
+answers_by TEXT NOT NULL,
+answers_at TIMESTAMP WITH TIME ZONE NOT NULL,
+topic TEXT NOT NULL,
+file_name TEXT NOT NULL,
+file_path TEXT NOT NULL,
+create_by TEXT NOT NULL,
+created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+updated_by TEXT NOT NULL,
+updated_at TIMESTAMP WITH TIME ZONE NOT NULL,
+deleted_by TEXT,
+deleted_at TIMESTAMP WITH TIME ZONE
 );
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
 -- SELECT 'down SQL query';
-DROP TABLE "pkp";
+DROP TABLE IF EXISTS "pkp";
 -- +goose StatementEnd
