9eff673699b5cb1b499fd4a9c94c5541cd3c2332
sofyan <sofyan@aqi.co.id>
2023-05-07 08:07:51 +0700

    db

--- db/migrations/20230410082314_pkp.sql
+++ db/migrations/20230410082314_pkp.sql
@@ -6,23 +6,23 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE public.pkp (
 id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-stakeholders TEXT NOT NULL,
-code TEXT NOT NULL,
-name TEXT NOT NULL,
-question_date TIMESTAMP WITH TIME ZONE NOT NULL,
-question TEXT NOT NULL,
-answers TEXT NOT NULL,
-answers_by TEXT NOT NULL,
-answers_at TIMESTAMP WITH TIME ZONE NOT NULL,
-topic TEXT NOT NULL,
-file_name TEXT NOT NULL,
-file_path TEXT NOT NULL,
-create_by TEXT NOT NULL,
-created_at TIMESTAMP WITH TIME ZONE NOT NULL,
-updated_by TEXT NOT NULL,
-updated_at TIMESTAMP WITH TIME ZONE NOT NULL,
-deleted_by TEXT,
-deleted_at TIMESTAMP WITH TIME ZONE
+stakeholders text NOT NULL,
+code text NOT NULL,
+name text NOT NULL,
+question_date timestamptz WITH TIME ZONE NULL,
+question text NOT NULL,
+answers text NOT NULL,
+answers_by text NOT NULL,
+answers_at timestamptz WITH TIME ZONE NOT NULL,
+topic text NOT NULL,
+file_name text NOT NULL,
+file_path text NOT NULL,
+create_by text NOT NULL,
+created_at timestamptz WITH TIME ZONE NOT NULL,
+updated_by text NOT NULL,
+updated_at timestamptz WITH TIME ZONE NOT NULL,
+deleted_by text,
+deleted_at timestamptz WITH TIME ZONE
 );
 -- +goose StatementEnd
 
