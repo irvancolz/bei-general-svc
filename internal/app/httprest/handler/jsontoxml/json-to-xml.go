e713ff4414294dc71a9979cd7ce09e85be9b6838
Shohieb <shohiebsense@users.noreply.github.com>
2023-05-19 08:29:25 +0700

    feat/add-json-to-xml-converter-endpoint

---
+++ internal/app/httprest/handler/jsontoxml/json-to-xml.go
@@ -0,0 +1,49 @@
+package jsontoxml
+
+import (
+	"be-idx-tsg/internal/app/httprest/model"
+	JsonToXmlUseCase "be-idx-tsg/internal/app/httprest/usecase/jsontoxml"
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Handler interface {
+	ToXml(c *gin.Context)
+}
+
+type handler struct {
+	jsonToXmlUseCase JsonToXmlUseCase.Usecase
+}
+
+func NewHandler() Handler {
+	return &handler{
+		JsonToXmlUseCase.ToXmlUseCase(),
+	}
+}
+
+func (jsonToXmlHandler *handler) ToXml(c *gin.Context) {
+
+	var data map[string]interface{}
+
+	err := c.ShouldBindJSON(&data)
+
+	if err != nil {
+		log.Println(err)
+		model.GenerateInvalidJsonResponse(c, err)
+	}
+
+	dataBytes, err := jsonToXmlHandler.jsonToXmlUseCase.ToXml(data)
+
+	if err != nil {
+		model.GenerateReadErrorResponse(c, err)
+	}
+
+	c.Writer.Header().Set("Content-Type", "application/xml")
+	_, err = c.Writer.Write(dataBytes)
+
+	if err != nil {
+		model.GenerateReadErrorResponse(c, err)
+	}
+	
+}
