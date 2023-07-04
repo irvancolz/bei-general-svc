d3eaaf9325efb93b2248f11206c840fa75c2c8a2
nugroho <nugrohowahidf22@tsgitdev.com>
2023-07-04 11:14:53 +0700

    add creator to announcements response

--- internal/app/httprest/model/announcement.go
+++ internal/app/httprest/model/announcement.go
@@ -1,24 +1,25 @@
 package model
 
 type Announcement struct {
-	ID              string `json:"id"`
-	InformationType string `json:"information_type"`
-	EffectiveDate   string `json:"effective_date"`
-	Regarding       string `json:"regarding"`
+	ID              string  `json:"id"`
+	InformationType string  `json:"information_type"`
+	EffectiveDate   string  `json:"effective_date"`
+	Regarding       string  `json:"regarding"`
 	Type            *string `json:"type"`
+	Creator         string  `json:"creator"`
 }
 
 type CreateAnnouncement struct {
-	InformationType string `json:"information_type" binding:"required,oneof='INTERNAL BURSA' 'AB' 'PARTICIPANT' 'PJSPPA' 'SEMUA' "`
-	EffectiveDate   string `json:"effective_date" binding:"required"`
-	Regarding       string `json:"regarding" binding:"required"`
+	InformationType string  `json:"information_type" binding:"required,oneof='INTERNAL BURSA' 'AB' 'PARTICIPANT' 'PJSPPA' 'SEMUA' "`
+	EffectiveDate   string  `json:"effective_date" binding:"required"`
+	Regarding       string  `json:"regarding" binding:"required"`
 	Type            *string `json:"type" binding:"required"`
 }
 
 type UpdateAnnouncement struct {
-	ID              string `json:"id" binding:"required"`
-	InformationType string `json:"information_type" binding:"required,oneof='INTERNAL BURSA' 'AB' 'PARTICIPANT' 'PJSPPA' 'SEMUA' "`
-	EffectiveDate   string `json:"effective_date" binding:"required"`
-	Regarding       string `json:"regarding" binding:"required"`
-	Type           *string `json:"type" binding:"required"`
+	ID              string  `json:"id" binding:"required"`
+	InformationType string  `json:"information_type" binding:"required,oneof='INTERNAL BURSA' 'AB' 'PARTICIPANT' 'PJSPPA' 'SEMUA' "`
+	EffectiveDate   string  `json:"effective_date" binding:"required"`
+	Regarding       string  `json:"regarding" binding:"required"`
+	Type            *string `json:"type" binding:"required"`
 }
