5159f8497e9d740681a3eacb667829527d895334
nugroho <nugrohowahidf22@tsgitdev.com>
2023-07-05 16:45:37 +0700

    add sync contact person to participant

--- internal/app/utilities/sync-data-src.go
+++ internal/app/utilities/sync-data-src.go
@@ -67,3 +67,51 @@ func GetLatestABCompanies(c *gin.Context) ([]model.ContactPersonSyncCompaniesRes
 
 	return result.Data, nil
 }
+
+func GetLatestParticipantCompanies(c *gin.Context) ([]model.ContactPersonSyncCompaniesResource, error) {
+	err_host := godotenv.Load(".env")
+	if err_host != nil {
+		fmt.Println(err_host)
+	}
+	host := os.Getenv("SERVICE_PARTICIPANT_HOST")
+
+	url := host + "/get-participant-to-sync-with-contact-person"
+	tokens, _ := c.Get("token")
+
+	Request, err := http.NewRequest("GET", url, nil)
+
+	Request.Header.Add("authorization", tokens.(string))
+	Request.Header.Set("Content-Type", "application/json")
+	if err != nil {
+		log.Println("failed to get data from : ", url)
+		log.Println("failed to get user email : ", err)
+		return nil, err
+	}
+
+	resp, err := http.DefaultClient.Do(Request)
+	if err != nil {
+		log.Println("failed to get user response: ", err)
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+	data, err := ioutil.ReadAll(resp.Body)
+
+	if resp.StatusCode != 200 {
+		log.Println("failed to get anggota bursa companies from ab list, error occured when try to get data")
+		return nil, errors.New("failed to get anggota bursa companies from ab list, error occured when try to get data")
+	}
+
+	if err != nil {
+		log.Println("failed to read data user results : ", err)
+		return nil, err
+	}
+
+	result := ContactpersonCompaniesSyncRequest{}
+	err_marshall := json.Unmarshal(data, &result)
+	if err_marshall != nil {
+		return nil, err_marshall
+	}
+
+	return result.Data, nil
+}
