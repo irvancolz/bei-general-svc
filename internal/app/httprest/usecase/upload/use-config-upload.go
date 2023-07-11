608a6ec61b42bb3100f9793464d370e6e9e59e8a
Sofyan Wanadi <sofyanwanadi6@gmail.com>
2023-07-11 17:41:08 +0700

    add lowercase in check ext file function

--- internal/app/httprest/usecase/upload/use-config-upload.go
+++ internal/app/httprest/usecase/upload/use-config-upload.go
@@ -20,7 +20,7 @@ func (c *UploadFileConfig) CheckFileExt(fileext string) bool {
 		return true
 	}
 	for _, ext := range c.Extensions {
-		result = ext == fileext
+		result = strings.ToLower(ext) == strings.ToLower(fileext)
 		if result {
 			return result
 		}
