86735b7342388d6166b269c4237ee24cef77400b
Shohieb <shohiebsense@users.noreply.github.com>
2023-08-30 14:02:45 +0700

    fix/add-xml-tag

--- internal/app/httprest/model/requestmodel/get_company_profile_xml_request.go
+++ internal/app/httprest/model/requestmodel/get_company_profile_xml_request.go
@@ -1,14 +1,13 @@
 package requestmodel
 
-//type: request/responnse
-//data: profile
-//extermnal type: "ab|participant|pjsppa|du
-//company code: kalo mau spesifik
-//syarat company code di isi, harus external typenya diisi juga
+// type: request/responnse
+// data: profile
+// extermnal type: "ab|participant|pjsppa|du
+// company code: kalo mau spesifik
+// syarat company code di isi, harus external typenya diisi juga
 type CompanyProfileXml struct {
-	Type string
-	Data string
-	ExternalType string
-	CompanyCode string
+	Type         string `xml:"Type"`
+	Data         string `xml:"Data"`
+	ExternalType string `xml:"ExternalType"`
+	CompanyCode  string `xml:"CompanyCode"`
 }
-
