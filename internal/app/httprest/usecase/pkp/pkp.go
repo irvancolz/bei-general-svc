6c90c7124a320eb168457ff4de68de99a72e9bdc
nugroho <nugrohowahidf22@tsgitdev.com>
2023-11-01 11:48:37 +0700

    update format in export pkp

--- internal/app/httprest/usecase/pkp/pkp.go
+++ internal/app/httprest/usecase/pkp/pkp.go
@@ -83,7 +83,7 @@ func (uc *usecase) GetAllPKuser(c *gin.Context) (*helper.PaginationResponse, err
 		exportedRows = append(exportedRows, strconv.Itoa(i+1))
 		exportedRows = append(exportedRows, helper.MapToArray(item, exportedFields)...)
 		for i, content := range exportedRows {
-			if helper.IsContains([]int{4, 6, 11}, i) {
+			if helper.IsContains([]int{4, 6}, i) {
 				unixTime, _ := strconv.Atoi(content)
 				dateTime := time.Unix(int64(unixTime), 0)
 				dateToFormat := helper.GetWIBLocalTime(&dateTime)
