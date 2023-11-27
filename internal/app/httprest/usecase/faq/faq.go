ef5efe2a8cb7e2cd2bd31a160b3a9f82f97fa7aa
Rifaldi Dwi <rifaldi.dwi@aqi.co.id>
2023-11-27 09:19:41 +0700

    fix: send notification on published faq only

--- internal/app/httprest/usecase/faq/faq.go
+++ internal/app/httprest/usecase/faq/faq.go
@@ -39,15 +39,17 @@ func (m *usecase) CreateFAQ(faq model.CreateFAQ, c *gin.Context, isDraft bool) (
 		return 0, err
 	}
 
-	internalBursaUser := email.GetAllUserInternalBursa(c)
-	var internalBursaUserId []string
-	for _, user := range internalBursaUser {
-		internalBursaUserId = append(internalBursaUserId, user.Id)
-	}
-	go utilities.CreateGroupNotif(c, internalBursaUserId, "FAQ", fmt.Sprintf("User %s menambahkan FAQ baru", c.GetString("name_user")))
+	if !isDraft {
+		internalBursaUser := email.GetAllUserInternalBursa(c)
+		var internalBursaUserId []string
+		for _, user := range internalBursaUser {
+			internalBursaUserId = append(internalBursaUserId, user.Id)
+		}
+		go utilities.CreateGroupNotif(c, internalBursaUserId, "FAQ", fmt.Sprintf("User %s menambahkan FAQ baru", c.GetString("name_user")))
 
-	for _, user := range internalBursaUser {
-		go email.SendEmailNotification(user, "Aktivitas Baru Di Menu FAQ", fmt.Sprintf("User %s menambahkan FAQ baru", c.GetString("name_user")))
+		for _, user := range internalBursaUser {
+			go email.SendEmailNotification(user, "Aktivitas Baru Di Menu FAQ", fmt.Sprintf("User %s menambahkan FAQ baru", c.GetString("name_user")))
+		}
 	}
 
 	return data, nil
@@ -74,7 +76,25 @@ func (m *usecase) DeleteFAQ(faqID string, c *gin.Context) (int64, error) {
 }
 
 func (m *usecase) UpdateStatusFAQ(faq model.UpdateFAQStatus, c *gin.Context) (int64, error) {
-	return m.faqRepo.UpdateStatusFAQ(faq, c)
+	data, err := m.faqRepo.UpdateStatusFAQ(faq, c)
+	if err != nil {
+		return 0, err
+	}
+
+	if faq.Status == model.PublishedFAQ {
+		internalBursaUser := email.GetAllUserInternalBursa(c)
+		var internalBursaUserId []string
+		for _, user := range internalBursaUser {
+			internalBursaUserId = append(internalBursaUserId, user.Id)
+		}
+		go utilities.CreateGroupNotif(c, internalBursaUserId, "FAQ", fmt.Sprintf("User %s menambahkan FAQ baru", c.GetString("name_user")))
+
+		for _, user := range internalBursaUser {
+			go email.SendEmailNotification(user, "Aktivitas Baru Di Menu FAQ", fmt.Sprintf("User %s menambahkan FAQ baru", c.GetString("name_user")))
+		}
+	}
+
+	return data, nil
 }
 
 func (m *usecase) UpdateFAQ(faq model.UpdateFAQ, c *gin.Context) (int64, error) {
