7b728a7a8fff10e29a0b724412cf877a0f742d4d
Rifaldi Dwi <rifaldi.dwi@aqi.co.id>
2023-11-27 09:13:56 +0700

    feat: add app & email notification in create & delete faq

--- internal/app/httprest/usecase/faq/faq.go
+++ internal/app/httprest/usecase/faq/faq.go
@@ -3,6 +3,9 @@ package faq
 import (
 	"be-idx-tsg/internal/app/httprest/model"
 	"be-idx-tsg/internal/app/httprest/repository/faq"
+	"be-idx-tsg/internal/app/utilities"
+	"be-idx-tsg/internal/pkg/email"
+	"fmt"
 
 	"github.com/gin-gonic/gin"
 )
@@ -31,11 +34,43 @@ func (m *usecase) GetAll(keyword, userId string) ([]*model.FAQ, error) {
 }
 
 func (m *usecase) CreateFAQ(faq model.CreateFAQ, c *gin.Context, isDraft bool) (int64, error) {
-	return m.faqRepo.CreateFAQ(faq, c, isDraft)
+	data, err := m.faqRepo.CreateFAQ(faq, c, isDraft)
+	if err != nil {
+		return 0, err
+	}
+
+	internalBursaUser := email.GetAllUserInternalBursa(c)
+	var internalBursaUserId []string
+	for _, user := range internalBursaUser {
+		internalBursaUserId = append(internalBursaUserId, user.Id)
+	}
+	go utilities.CreateGroupNotif(c, internalBursaUserId, "FAQ", fmt.Sprintf("User %s menambahkan FAQ baru", c.GetString("name_user")))
+
+	for _, user := range internalBursaUser {
+		go email.SendEmailNotification(user, "Aktivitas Baru Di Menu FAQ", fmt.Sprintf("User %s menambahkan FAQ baru", c.GetString("name_user")))
+	}
+
+	return data, nil
 }
 
 func (m *usecase) DeleteFAQ(faqID string, c *gin.Context) (int64, error) {
-	return m.faqRepo.DeleteFAQ(faqID, c)
+	data, err := m.faqRepo.DeleteFAQ(faqID, c)
+	if err != nil {
+		return 0, err
+	}
+
+	internalBursaUser := email.GetAllUserInternalBursa(c)
+	var internalBursaUserId []string
+	for _, user := range internalBursaUser {
+		internalBursaUserId = append(internalBursaUserId, user.Id)
+	}
+	go utilities.CreateGroupNotif(c, internalBursaUserId, "FAQ", fmt.Sprintf("User %s menghapus FAQ", c.GetString("name_user")))
+
+	for _, user := range internalBursaUser {
+		go email.SendEmailNotification(user, "Aktivitas Baru Di Menu FAQ", fmt.Sprintf("User %s menghapus FAQ", c.GetString("name_user")))
+	}
+
+	return data, nil
 }
 
 func (m *usecase) UpdateStatusFAQ(faq model.UpdateFAQStatus, c *gin.Context) (int64, error) {
